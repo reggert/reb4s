8dc9abac2d6d9769ba93c9b1bde1d424d30b783a
reggert <reggert@c3790249-f406-440c-8f77-83ccbb905bc9>
2012-01-22 23:58:24 +0000

    finished Quantifiable trait
    git-svn-id: https://reb4j.svn.sourceforge.net/svnroot/reb4j/trunk@82 c3790249-f406-440c-8f77-83ccbb905bc9

--- src/main/scala/net/sourceforge/reb4j/scala/experimental/Quantifiable.scala
+++ src/main/scala/net/sourceforge/reb4j/scala/experimental/Quantifiable.scala
@@ -1,6 +1,5 @@
 package net.sourceforge.reb4j.scala.experimental
 
-@SerialVersionUID(1L)
 trait Quantifiable extends Expression
 {
 	def * = new Quantified(this, "*")
@@ -28,5 +27,21 @@ trait Quantifiable extends Expression
 	def ##? (n : Int) = new Quantified(this, "{" + n + "}?")
 	def ##+ (n : Int) = new Quantified(this, "{" + n + "}+")
 	def repeat (n : Int) = ## (n)
+	def repeatReluctantly (n : Int) = ##? (n)
+	def repeatPossessively (n : Int) = ##+ (n)
+	
+	def ## (min : Int, max : Int) = new Quantified(this, "{" + min + "," + max + "}")
+	def ##? (min : Int, max : Int) = new Quantified(this, "{" + min + "," + max + "}?")
+	def ##+ (min : Int, max : Int) = new Quantified(this, "{" + min + "," + max + "}+")
+	def repeat (min : Int, max : Int) = ## (min, max)
+	def repeatReluctantly (min : Int, max : Int) = ##? (min, max)
+	def repeatPossessively (min : Int, max : Int) = ##+ (min, max)
+	
+	def ##> (n : Int) = new Quantified(this, "{" + n + ",}")
+	def ##>? (n : Int) = new Quantified(this, "{" + n + ",}?")
+	def ##>+ (n : Int) = new Quantified(this, "{" + n + ",}+")
+	def atLeast (n : Int) = ##> (n)
+	def atLeastReluctantly (n : Int) = ##>? (n)
+	def atLeastPossessively (n : Int) = ##>+ (n)
 	
 }
