30a7fa61e4247342eb068bf8efe29347fe40d8ef
reggert <reggert@c3790249-f406-440c-8f77-83ccbb905bc9>
2012-01-22 07:45:52 +0000

    added more operators
    git-svn-id: https://reb4j.svn.sourceforge.net/svnroot/reb4j/trunk@76 c3790249-f406-440c-8f77-83ccbb905bc9

--- src/main/scala/net/sourceforge/reb4j/scala/Regex.scala
+++ src/main/scala/net/sourceforge/reb4j/scala/Regex.scala
@@ -6,24 +6,43 @@ class Regex protected[scala] (val expression : String)
 	if (expression == null)
 		throw new NullPointerException("expression");
 	
-	override def toString() = expression
+	override def toString = expression
 	
 	def this(pattern : Pattern) = this(pattern.toString())
 	
-	def toPattern() = Pattern.compile(expression)
+	def toPattern = Pattern.compile(expression)
 	def toPattern(flags : Int) = Pattern.compile(expression, flags)
 	
-	def or(right : Regex*) = right.addString(new StringBuilder(expression), "|")
+	def or(right : Regex*) = right addString(new StringBuilder(expression), "|")
 	def | (right : Regex) = or(right)
 	
 	def then(right : Regex*) = right addString new StringBuilder(expression)
-	def :: (right : Regex) = then(right)
+	def :: (left : Regex) = left then this
 	
-	def * () = new Regex(delimit() + '*')
-	def + () = new Regex(delimit() + '+')
+	def * = new Regex(delimit + '*')
+	def *? = new Regex(delimit + "*?")
+	def *+ = new Regex(delimit + "*+")
 	
-	private def delimit() = 
-		(new StringBuilder("(?:") append expression append (')')).toString()
+	def + = new Regex(delimit + '+')
+	def +? = new Regex(delimit + "+?")
+	def ++ = new Regex(delimit + "++")
+	
+	def ## (n : Int) = new Regex(delimit + "{" + n + "}")
+	def ##? (n : Int) = new Regex(delimit + "{" + n + "}?")
+	def ##+ (n : Int) = new Regex(delimit + "{" + n + "}+")
+	def ## (min : Int, max : Int) = new Regex(delimit + "{" + min + "," + max + "}")
+	def ##? (min : Int, max : Int) = new Regex(delimit + "{" + min + "," + max + "}?")
+	def ##+ (min : Int, max : Int) = new Regex(delimit + "{" + min + "," + max + "}+")
+	def ##> (n : Int) = new Regex(delimit + "{" + n + ",}")
+	def ##>? (n : Int) = new Regex(delimit + "{" + n + ",}?")
+	def ##>+ (n : Int) = new Regex(delimit + "{" + n + ",}+")
+	
+	def backReference (group : Int) = 
+		if (group > 0) "\\" + group
+		else throw new IllegalArgumentException("group <= 0") 
+	
+	private def delimit = 
+		(new StringBuilder("(?:") append expression append (')')).toString
 }
 
 
@@ -35,7 +54,7 @@ object Literal
 	
 	private val needsEscape = "()[]{}.,-\\|+*?$^&:!<>="
 	private def escapeChar(c : Char) = if (needsEscape.contains(c)) "\\" + c else String.valueOf(c)
-	private def escape(literal : String) = (literal.map(escapeChar) addString (new StringBuilder)).toString()
+	private def escape(literal : String) = (literal.map(escapeChar) addString (new StringBuilder)).toString
 }
 
 
