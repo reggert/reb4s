32d26d68542a689397506cea2feb95df76a19f1d
reggert <reggert@c3790249-f406-440c-8f77-83ccbb905bc9>
2012-01-23 00:39:36 +0000

    implemented alternation and started work on sequences
    git-svn-id: https://reb4j.svn.sourceforge.net/svnroot/reb4j/trunk@83 c3790249-f406-440c-8f77-83ccbb905bc9

--- net.sourceforge.reb4j.scala/src/main/scala/net/sourceforge/reb4j/scala/experimental/Atomic.scala
+++ net.sourceforge.reb4j.scala/src/main/scala/net/sourceforge/reb4j/scala/experimental/Atomic.scala
@@ -2,5 +2,6 @@ package net.sourceforge.reb4j.scala.experimental
 
 trait Atomic extends Expression
 {
-
+	def | (right : Alternation) = new Alternation(this +: right.alternatives)
+	def | (right : Atomic) = new Alternation(List(this, right))
 }
--- net.sourceforge.reb4j.scala/src/main/scala/net/sourceforge/reb4j/scala/experimental/Quantified.scala
+++ net.sourceforge.reb4j.scala/src/main/scala/net/sourceforge/reb4j/scala/experimental/Quantified.scala
@@ -1,7 +1,7 @@
 package net.sourceforge.reb4j.scala.experimental
 
 class Quantified private[experimental] (val base : Quantifiable, val quantifier : String) 
-	extends Expression
+	extends Expression with Atomic
 {
 	lazy val expression = base.toString() + quantifier
 	override def toString = expression
