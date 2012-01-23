e1ed49814628b551823106a430f5e9ea7079d120
reggert <reggert@c3790249-f406-440c-8f77-83ccbb905bc9>
2012-01-23 00:39:36 +0000

    implemented alternation and started work on sequences
    git-svn-id: https://reb4j.svn.sourceforge.net/svnroot/reb4j/trunk@83 c3790249-f406-440c-8f77-83ccbb905bc9

--- src/main/scala/net/sourceforge/reb4j/scala/experimental/Literal.scala
+++ src/main/scala/net/sourceforge/reb4j/scala/experimental/Literal.scala
@@ -1,10 +1,11 @@
 package net.sourceforge.reb4j.scala.experimental
 
 @SerialVersionUID(1L)
-class Literal(val literal : String) extends Expression with Atomic
+class Literal(val literal : String) extends Expression with Atomic with Quantifiable
 {
 	lazy val escaped = Literal.escape(literal)
 	override def toString = escaped
+	def + (right : Literal) = new Literal(literal + right.literal)
 }
 
 
--- src/main/scala/net/sourceforge/reb4j/scala/experimental/Sequence.scala
+++ src/main/scala/net/sourceforge/reb4j/scala/experimental/Sequence.scala
@@ -1,6 +1,11 @@
 package net.sourceforge.reb4j.scala.experimental
 
-class Sequence extends Expression 
+final class Sequence private[experimental] (val components : List[Atomic]) 
+	extends Expression with Atomic
 {
-
+	lazy val expression = (components addString new StringBuilder).toString
+	override def toString = expression
+	
+	def + (right : Sequence) = new Sequence(components ++ right.components)
+	def + (right : Atomic) = new Sequence(components :+ right)
 }
