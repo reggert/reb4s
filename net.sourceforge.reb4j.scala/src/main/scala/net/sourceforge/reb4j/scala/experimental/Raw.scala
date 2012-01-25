cc39acf896173e2c9a5d0f3085434ded835327ed
reggert <reggert@c3790249-f406-440c-8f77-83ccbb905bc9>
2012-01-25 02:52:45 +0000

    renamed Verbatim to Raw
    git-svn-id: https://reb4j.svn.sourceforge.net/svnroot/reb4j/trunk@89 c3790249-f406-440c-8f77-83ccbb905bc9

---
+++ net.sourceforge.reb4j.scala/src/main/scala/net/sourceforge/reb4j/scala/experimental/Raw.scala
@@ -0,0 +1,25 @@
+package net.sourceforge.reb4j.scala.experimental
+
+case class Raw private[experimental] (val expression : String) 
+	extends Expression with Alternative with Quantifiable
+{
+	override def toString = expression
+	override def + (right : Alternative) = right match
+	{
+		case Raw(rhs) => new Raw(expression + rhs)
+		case right : Literal => new Raw(expression + right.escaped)
+	}
+	def + (right : Raw) = new Raw(expression + right.expression)
+	def + (right : Literal) = new Raw(expression + right.escaped)
+}
+
+
+object AnyChar extends Raw(".")
+object LineBegin extends Raw("^")
+object LineEnd extends Raw("$")
+object WordBoundary extends Raw("\\b")
+object NonwordBoundary extends Raw("\\B")
+object InputBegin extends Raw("\\A")
+object MatchEnd extends Raw("\\G")
+object InputEndSkipEOL extends Raw("\\Z")
+object InputEnd extends Raw("\\z")
