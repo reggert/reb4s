76072139f3c4061599b7316804cd1a9353cf7801
reggert <reggert@c3790249-f406-440c-8f77-83ccbb905bc9>
2012-01-25 01:53:06 +0000

    began favoring overriding over overloading
    git-svn-id: https://reb4j.svn.sourceforge.net/svnroot/reb4j/trunk@87 c3790249-f406-440c-8f77-83ccbb905bc9

--- net.sourceforge.reb4j.scala/src/main/scala/net/sourceforge/reb4j/scala/experimental/Sequence.scala
+++ net.sourceforge.reb4j.scala/src/main/scala/net/sourceforge/reb4j/scala/experimental/Sequence.scala
@@ -1,11 +1,14 @@
 package net.sourceforge.reb4j.scala.experimental
 
-final class Sequence private[experimental] (val components : List[Alternative]) 
+final case class Sequence private[experimental] (val components : List[Alternative]) 
 	extends Expression with Alternative
 {
 	lazy val expression = (components addString new StringBuilder).toString
 	override def toString = expression
 	
-	override def + (right : Sequence) = new Sequence(components ++ right.components)
-	def + (right : Alternative) = new Sequence(components :+ right)
+	override def + (right : Alternative) = right match
+	{
+		case Sequence(rhs) => new Sequence(components ++ rhs)
+		case _ => new Sequence(components :+ right)
+	}
 }
