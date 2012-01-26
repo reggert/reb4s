a1ddf29d95247876a50293d85a62c127de2d2523
reggert <reggert@c3790249-f406-440c-8f77-83ccbb905bc9>
2012-01-26 06:28:43 +0000

    renamed alternation operation to avoid conflict with scala match alternatives
    git-svn-id: https://reb4j.svn.sourceforge.net/svnroot/reb4j/trunk@95 c3790249-f406-440c-8f77-83ccbb905bc9

--- net.sourceforge.reb4j.scala/src/main/scala/net/sourceforge/reb4j/scala/experimental/Alternative.scala
+++ net.sourceforge.reb4j.scala/src/main/scala/net/sourceforge/reb4j/scala/experimental/Alternative.scala
@@ -2,8 +2,8 @@ package net.sourceforge.reb4j.scala.experimental
 
 trait Alternative extends Expression
 {
-	def | (right : Alternation) = new Alternation(this +: right.alternatives)
-	def | (right : Alternative) = new Alternation(List(this, right))
+	def || (right : Alternation) = new Alternation(this +: right.alternatives)
+	def || (right : Alternative) = new Alternation(List(this, right))
 	def + (right : Alternative) : Alternative = right match
 	{
 		case Sequence(rhs) => new Sequence(this::rhs)
