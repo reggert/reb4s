81465d2f27fb8299775f75e11424f0e66cd8733c
reggert <reggert@c3790249-f406-440c-8f77-83ccbb905bc9>
2012-01-26 06:28:43 +0000

    renamed alternation operation to avoid conflict with scala match alternatives
    git-svn-id: https://reb4j.svn.sourceforge.net/svnroot/reb4j/trunk@95 c3790249-f406-440c-8f77-83ccbb905bc9

--- src/main/scala/net/sourceforge/reb4j/scala/experimental/Alternation.scala
+++ src/main/scala/net/sourceforge/reb4j/scala/experimental/Alternation.scala
@@ -6,6 +6,6 @@ final class Alternation(val alternatives : List[Alternative]) extends Expression
 	lazy val expression = (alternatives addString new StringBuilder).toString
 	override def toString = expression
 	
-	def | (right : Alternation) = new Alternation(alternatives ++ right.alternatives)
-	def | (right : Alternative) = new Alternation(alternatives :+ right)
+	def || (right : Alternation) = new Alternation(alternatives ++ right.alternatives)
+	def || (right : Alternative) = new Alternation(alternatives :+ right)
 }
