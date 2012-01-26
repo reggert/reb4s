595a16ddc1e1656ac727746709305b41882712f2
reggert <reggert@c3790249-f406-440c-8f77-83ccbb905bc9>
2012-01-26 06:11:46 +0000

    renamed Literal.literal to Literal.unescaped
    git-svn-id: https://reb4j.svn.sourceforge.net/svnroot/reb4j/trunk@93 c3790249-f406-440c-8f77-83ccbb905bc9

--- src/main/scala/net/sourceforge/reb4j/scala/experimental/Literal.scala
+++ src/main/scala/net/sourceforge/reb4j/scala/experimental/Literal.scala
@@ -1,18 +1,18 @@
 package net.sourceforge.reb4j.scala.experimental
 
 @SerialVersionUID(1L)
-case class Literal(val literal : String) extends Expression 
+case class Literal(val unescaped : String) extends Expression 
 	with Alternative with Quantifiable
 {
-	lazy val escaped = Literal.escape(literal)
+	lazy val escaped = Literal.escape(unescaped)
 	override def toString = escaped
 	override def + (right : Alternative) = right match
 	{
-		case Literal(rhs) => new Literal(literal + rhs)
+		case Literal(rhs) => new Literal(unescaped + rhs)
 		case Raw(rhs) => new Raw(escaped + rhs)
 		case _ => super.+(right)
 	}
-	def + (right : Literal) = new Literal(literal + right.literal)
+	def + (right : Literal) = new Literal(unescaped + right.unescaped)
 }
 
 
@@ -20,6 +20,6 @@ object Literal
 {
 	val needsEscape = "()[]{}.,-\\|+*?$^&:!<>="
 	def escapeChar(c : Char) = if (needsEscape.contains(c)) "\\" + c else String.valueOf(c)
-	def escape(literal : Seq[Char]) = 
-		(literal.map(escapeChar) addString (new StringBuilder)).toString
+	def escape(unescaped : Seq[Char]) = 
+		(unescaped.map(escapeChar) addString (new StringBuilder)).toString
 }
