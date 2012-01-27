5ceb94caa1fe1572a18578c181c28b44312850e4
reggert <reggert@c3790249-f406-440c-8f77-83ccbb905bc9>
2012-01-27 04:36:52 +0000

    fixed quantifier rules
    git-svn-id: https://reb4j.svn.sourceforge.net/svnroot/reb4j/trunk@100 c3790249-f406-440c-8f77-83ccbb905bc9

--- src/main/scala/net/sourceforge/reb4j/scala/experimental/Raw.scala
+++ src/main/scala/net/sourceforge/reb4j/scala/experimental/Raw.scala
@@ -2,7 +2,7 @@ package net.sourceforge.reb4j.scala.experimental
 
 @SerialVersionUID(1L)
 case class Raw private[experimental] (val expression : String) 
-	extends Expression with Alternative with Quantifiable
+	extends Expression with Alternative 
 {
 	override def toString = expression
 	override def + (right : Alternative) = right match
@@ -17,13 +17,12 @@ case class Raw private[experimental] (val expression : String)
 	def then (right : Literal) = this + right
 }
 
-
-object AnyChar extends Raw(".")
-object LineBegin extends Raw("^")
-object LineEnd extends Raw("$")
-object WordBoundary extends Raw("\\b")
-object NonwordBoundary extends Raw("\\B")
-object InputBegin extends Raw("\\A")
-object MatchEnd extends Raw("\\G")
-object InputEndSkipEOL extends Raw("\\Z")
-object InputEnd extends Raw("\\z")
+object AnyChar extends Raw(".") with Quantifiable
+object LineBegin extends Raw("^") with Quantifiable
+object LineEnd extends Raw("$") with Quantifiable
+object WordBoundary extends Raw("\\b") with Quantifiable
+object NonwordBoundary extends Raw("\\B") with Quantifiable
+object InputBegin extends Raw("\\A") with Quantifiable
+object MatchEnd extends Raw("\\G") with Quantifiable
+object InputEndSkipEOL extends Raw("\\Z") with Quantifiable
+object InputEnd extends Raw("\\z") with Quantifiable
