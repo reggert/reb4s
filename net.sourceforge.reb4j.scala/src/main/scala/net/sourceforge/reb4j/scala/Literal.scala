1b31edea817138d7fbdba434ddf6a006c094f288
reggert <reggert@c3790249-f406-440c-8f77-83ccbb905bc9>
2012-02-05 06:52:52 +0000

    started work on documentation
    git-svn-id: https://reb4j.svn.sourceforge.net/svnroot/reb4j/trunk@120 c3790249-f406-440c-8f77-83ccbb905bc9

--- net.sourceforge.reb4j.scala/src/main/scala/net/sourceforge/reb4j/scala/Literal.scala
+++ net.sourceforge.reb4j.scala/src/main/scala/net/sourceforge/reb4j/scala/Literal.scala
@@ -1,15 +1,41 @@
 package net.sourceforge.reb4j.scala
 
+/**
+ * Expression that matches a specific string.
+ */
 @SerialVersionUID(1L)
 sealed class Literal private[scala] (val unescaped : String) extends Expression 
 	with Alternation.Alternative
 	with Sequence.Sequenceable
 {
 	require (unescaped != null, "unescaped is null")
+	
+	/**
+	 * The string in escaped form.
+	 */
 	lazy val escaped = Literal.escape(unescaped)
 	override def expression = escaped
+	
+	/**
+	 * Concatenates this literal with the argument.
+	 */
 	final def + (right : Literal) = new Literal(unescaped + right.unescaped)
+	
+	/**
+	 * Concatenates this literal with the argument.
+	 */
 	final def then (right : Literal) = this + right
+	
+	/**
+	 * Concatenates this literal with the specified raw expression.
+	 */
+	final def + (right : Raw) = new Raw(escaped + right.expression)
+	
+	/**
+	 * Concatenates this literal with the specified raw expression.
+	 */
+	final def then (right : Raw) = this + right
+	
 	override def equals(other : Any) = other match
 	{
 		case that : Literal => this.unescaped == that.unescaped
@@ -19,6 +45,9 @@ sealed class Literal private[scala] (val unescaped : String) extends Expression
 }
 
 
+/**
+ * Constructs a literal expression.
+ */
 object Literal
 {
 	def apply(unescaped : Char) =
