1935cc69b06e558fdf972bb38277d52a1c51f508
reggert <reggert@c3790249-f406-440c-8f77-83ccbb905bc9>
2012-02-05 19:35:16 +0000

    documented CharClass
    git-svn-id: https://reb4j.svn.sourceforge.net/svnroot/reb4j/trunk@125 c3790249-f406-440c-8f77-83ccbb905bc9

--- net.sourceforge.reb4j.scala/src/main/scala/net/sourceforge/reb4j/scala/charclass/CharClass.scala
+++ net.sourceforge.reb4j.scala/src/main/scala/net/sourceforge/reb4j/scala/charclass/CharClass.scala
@@ -2,64 +2,205 @@ package net.sourceforge.reb4j.scala.charclass
 import java.lang.Character.UnicodeBlock
 import net.sourceforge.reb4j.scala.{Expression, Alternation, Quantifiable, Sequence}
 
+/**
+ * Base class representing an expression that matches a single character 
+ * within a class of characters.
+ */
 @SerialVersionUID(1L)
 abstract class CharClass extends Expression 
 	with Alternation.Alternative
 	with Sequence.Sequenceable
 	with Quantifiable
 {
+	/**
+	 * Returns an expressing matching a single character that is not within
+	 * the class of characters matched by this expression.
+	 */
 	def ^ : CharClass
+	
+	/**
+	 * Returns an expressing matching a single character that is not within
+	 * the class of characters matched by this expression.
+	 */
 	final def negate = ^
 	
+	/**
+	 * The regular expression string that can be used within square brackets
+	 * to merge with other character classes.
+	 */
 	protected[charclass] def unitableForm : String
+	
+	/**
+	 * The regular expression string that can be used independently of square 
+	 * brackets.
+	 */
 	protected[charclass] def independentForm : String
+	
 	final override def expression = independentForm
 }
 
 
+/**
+ * Factory object for creating character class expressions.
+ */
 object CharClass
 {
+	/**
+	 * Creates an expression matching a single specified character.
+	 */
 	def char(c : Char) = new SingleChar(c)
+	
+	/**
+	 * Creates an expression that matches a single occurrence of any of the 
+	 * specified characters.
+	 */
 	def chars(cs : Set[Char]) = new MultiChar(cs)
+	
+	/**
+	 * Creates an expression that matches a single occurrence of any of the 
+	 * specified characters.
+	 */
 	def chars(cs : Seq[Char]) = new MultiChar(Set.empty ++ cs)
+	
+	/**
+	 * Creates an expression that matches a single occurence of any character
+	 * within the specified range.
+	 * 
+	 * @param first the lowest value character that will match
+	 * @param last the highest value character that will match
+	 * @throws IllegalArgumentException if {first} is greater than {last}.
+	 */
 	def range(first : Char, last : Char) = new CharRange(first, last)
 	
+	/**
+	 * Module containing Perl-style predefined character classes.
+	 */
 	object Perl
 	{
+		/**
+		 * Perl-style character class that matches a single decimal digit.
+		 */
 		val Digit = new PredefinedClass('d')
+		
+		/**
+		 * Perl-style character class that matches whitespace.
+		 */
 		val Space = new PredefinedClass('s')
+		
+		/**
+		 * Perl-style character class that matches "word" characters.
+		 */
 		val Word = new PredefinedClass('w')
 	}
 	
+	/**
+	 * Module containing POSIX-style predefined character classes.
+	 */
 	object Posix
 	{
+		/**
+		 * POSIX-style character class that matches a lowercase letter.
+		 */
 		val Lower = new NamedPredefinedClass("Lower")
+		
+		/**
+		 * POSIX-style character class that matches an uppercase letter.
+		 */
 		val Upper = new NamedPredefinedClass("Upper")
+		
+		/**
+		 * POSIX-style character class that matches an alphabetical letter.
+		 */
 		val Alpha = new NamedPredefinedClass("Alpha")
+		
+		/**
+		 * POSIX-style character class that matches a decimal digit.
+		 */
 		val Digit = new NamedPredefinedClass("Digit")
+		
+		/**
+		 * POSIX-style character class that matches letters or digits.
+		 */
 		val Alnum = new NamedPredefinedClass("Alnum")
+		
+		/**
+		 * POSIX-style character class that matches a punctuation character.
+		 */
 		val Punct = new NamedPredefinedClass("Punct")
+		
+		/**
+		 * POSIX-style character class that matches a graphical character.
+		 */
 		val Graph = new NamedPredefinedClass("Graph")
+		
+		/**
+		 * POSIX-style character class that matches any printable character.
+		 */
 		val Print = new NamedPredefinedClass("Print")
+		
+		/**
+		 * POSIX-style character class that matches a space or tab.
+		 */
 		val Blank = new NamedPredefinedClass("Blank")
+		
+		/**
+		 * POSIX-style character class that matches a control character.
+		 */
 		val Control = new NamedPredefinedClass("Cntrl")
+		
+		/**
+		 * POSIX-style character class that matches a hexadecimal digit.
+		 */
 		val HexDigit = new NamedPredefinedClass("XDigit")
+		
+		/**
+		 * POSIX-style character class that matches any whitespace character.
+		 */
 		val Space = new NamedPredefinedClass("Space")
 	}
 	
+	/**
+	 * Module containing predefined character classes matching character traits
+	 * defined by the [[java.lang.Character]] class.
+	 */
 	object Java
 	{
+		/**
+		 * Matches any single lowercase letter.
+		 */
 		val LowerCase = new NamedPredefinedClass("javaLowerCase")
+		
+		/**
+		 * Matches any single uppercase letter.
+		 */
 		val UpperCase = new NamedPredefinedClass("javaUpperCase")
+		
+		/**
+		 * Matches any single whitespace character.
+		 */
 		val Whitespace = new NamedPredefinedClass("javaWhitespace")
+		
+		/**
+		 * Matches any single "mirrored" character.
+		 */
 		val Mirrored = new NamedPredefinedClass("javaMirrored")
 	}
 	
+	/**
+	 * Module containing predefined character classes matching a single
+	 * character that has certain traits defined by the Unicode specification.
+	 */
 	object Unicode
 	{
 		private type UnicodeBlock = java.lang.Character.UnicodeBlock
 		private def % (className : String) = new NamedPredefinedClass(className)
+		
+		/**
+		 * Creates a character class matching any single character within the
+		 * specified Unicode block.
+		 */
 		def block(unicodeBlock : UnicodeBlock) = %("In" + unicodeBlock.toString)
+		
 		/*
 			From the Unicode Specification, version 4.0.0, the Unicode categories are:
 				Lu = Letter, uppercase
@@ -93,61 +234,206 @@ object CharClass
 				Sk = Symbol, modifier
 				So = Symbol, other
 		 */
+		
+		/**
+		 * Module containing predefined character classes matching any single
+		 * character defined as a "letter" by the Unicode specification.
+		 */
 		object Letter
 		{
+			/**
+			 * Matches uppercase letters.
+			 */
 			val Uppercase = %("Lu")
+			
+			/**
+			 * Matches lowercase letters.
+			 */
 			val Lowercase = %("Ll")
+			
+			/**
+			 * Matches titlecase letters.
+			 */
 			val Titlecase = %("Lt")
+			
+			/**
+			 * Matches letter modifiers.
+			 */
 			val Modifier = %("Lm")
+			
+			/**
+			 * Matches "other" letters defined by Unicode.
+			 */
 			val Other = %("Lo")
 		}
 		
+		/**
+		 * Module containing predefined character classes matching any single
+		 * character defined as a "mark" by the Unicode specification.
+		 */
 		object Mark
 		{
+			/**
+			 * Matches nonspacing marks.
+			 */
 			val Nonspacing = %("Mn")
+			
+			/**
+			 * Matches spacing-combining marks.
+			 */
 			val SpacingCombining = %("Mc")
+			
+			/**
+			 * Matches enclosing marks.
+			 */
 			val Enclosing = %("Me")
 		}
 		
+		/**
+		 * Module containing predefined character classes matching any single
+		 * character defined as a "number" by the Unicode specification.
+		 */
 		object Number
 		{
+			/**
+			 * Matches decimal digits.
+			 */
 			val DecimalDigit = %("Nd")
+			
+			/**
+			 * Matches letter characters used as digits.
+			 */
 			val Letter = %("Nl")
+			
+			/**
+			 * Matches "other" digit characters defined by Unicode.
+			 */
 			val Other = %("No")
 		}
 		
+		/**
+		 * Module containing predefined character classes matching any single
+		 * character defined as a "separator" by the Unicode specification.
+		 */
 		object Separator
 		{
+			/**
+			 * Mataches spaces.
+			 */
 			val Space = %("Zs")
+			
+			/**
+			 * Matches line breaks.
+			 */
 			val Line = %("Zl")
+			
+			/**
+			 * Matches paragraph breaks.
+			 */
 			val Paragraph = %("Zp")
 		}
 		
+		/**
+		 * Module containing predefined character classes matching any single
+		 * character that does not fit into any other category defined by the
+		 * Unicode specification.
+		 */
 		object Other
 		{
+			/**
+			 * Matches control characters.
+			 */
 			val Control = %("Cc")
+			
+			/**
+			 * Matches formatting characters.
+			 */
 			val Format = %("Cf")
+			
+			/**
+			 * Matches surrogate characters.
+			 */
 			val Surrogate = %("Cs")
+			
+			/**
+			 * Matches characters defined for private use.
+			 */
 			val PrivateUse = %("Co")
+			
+			/**
+			 * Matches unassigned characters.
+			 */
 			val NotAssigned = %("Cn")
 		}
 		
+		/**
+		 * Module containing predefined character classes matching any single
+		 * character that is defined as "punctuation" by the Unicode 
+		 * specification.
+		 */
 		object Punctuation
 		{
+			/**
+			 * Matches connectors.
+			 */
 			val Connector = %("Pc")
+			
+			/**
+			 * Matches dashes.
+			 */
 			val Dash = %("Pd")
+			
+			/**
+			 * Matches "opening" punctuation.
+			 */
 			val Open = %("Po")
+			
+			/**
+			 * Matches "closing" punctuation.
+			 */
 			val Close = %("Pe")
+			
+			/**
+			 * Matches initial quotes.
+			 */
 			val InitialQuote = %("Pi")
+			
+			/**
+			 * Matches closing quotes.
+			 */
 			val FinalQuote = %("Pf")
+			
+			/**
+			 * Matches other punctuation.
+			 */
 			val Other = %("Po")
 		}
 		
+		/**
+		 * Module containing predefined character classes matching any single
+		 * character that is defined as a "symbol" by the Unicode 
+		 * specification.
+		 */
 		object Symbol
 		{
+			/**
+			 * Matches mathematical symbols.
+			 */
 			val Math = %("Sm")
+			
+			/**
+			 * Matches currency symbols.
+			 */
 			val Currency = %("Sc")
+			
+			/**
+			 * Matches symbol modifiers.
+			 */
 			val Modifier = %("Sk")
+			
+			/**
+			 * Matches other symbols.
+			 */
 			val Other = %("So")
 		}
 	}
