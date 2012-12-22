ffa27a4f164a8324afaf45dee6a1815b7bbd78e5
Richard W. Eggert II <richard.eggert@gmail.com>
2012-12-22 17:02:13 -0500

    blew away old api - GitHub ticket #5

--- net.sourceforge.reb4j/src/main/java/net/sourceforge/reb4j/charclass/package-info.java
+++ net.sourceforge.reb4j/src/main/java/net/sourceforge/reb4j/charclass/package-info.java
@@ -16,25 +16,8 @@
 	You should have received a copy of the GNU Lesser General Public License
 	along with reb4j.  If not, see <http://www.gnu.org/licenses/>.
  */
-package net.sourceforge.reb4j;
-
 /**
- * This enumeration represents flags that may be embedded into regular 
- * expressions.
- * 
- * @author Richard W. Eggert II
- * @see Regex#enable(Flag...)
- * @see Regex#disable(Flag...)
- * @see Regex#enableDirective(Flag...)
- * @see Regex#disableDirective(Flag...)
- *
+ * Regular Expression Builder for Java
+ * Character Class Routines
  */
-public enum Flag
-{
-	CASE_INSENSITIVE, // 'i'
-	UNIX_LINES, // 'd'
-	MULTILINE, // 'm'
-	DOTALL, // 's'
-	UNICODE_CASE, // 'u' 
-	COMMENTS // 'x'
-}
+package net.sourceforge.reb4j.charclass;
