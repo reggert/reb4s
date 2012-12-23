73a98c4c4504fbda62d38167f2fbef9dfc92351e
Richard W. Eggert II <richard.eggert@gmail.com>
2012-12-23 02:30:55 -0500

    implemented Sequence - GitHub ticket #5

---
+++ net.sourceforge.reb4j/src/main/java/net/sourceforge/reb4j/Sequence.java
@@ -0,0 +1,81 @@
+package net.sourceforge.reb4j;
+
+import fj.F2;
+import fj.data.List;
+import net.sourceforge.reb4j.Alternation.Alternative;
+
+public final class Sequence extends AbstractExpression
+	implements Alternation.Alternative, SequenceOps
+{
+	private static final long serialVersionUID = 1L;
+	private final List<Sequenceable> components;
+	
+	public Sequence(final Sequence left, final Sequence right)
+	{
+		if (left == null) throw new NullPointerException("left");
+		if (right == null) throw new NullPointerException("right");
+		this.components = left.components.append(right.components);
+	}
+	
+	public Sequence(final Sequence left, final Sequenceable right)
+	{
+		if (left == null) throw new NullPointerException("left");
+		if (right == null) throw new NullPointerException("right");
+		this.components = left.components.append(List.single(right));
+	}
+	
+	public Sequence(final Sequenceable left, final Sequence right)
+	{
+		if (left == null) throw new NullPointerException("left");
+		if (right == null) throw new NullPointerException("right");
+		this.components = right.components.cons(left);
+	}
+	
+	public Sequence(final Sequenceable left, final Sequenceable right)
+	{
+		if (left == null) throw new NullPointerException("left");
+		if (right == null) throw new NullPointerException("right");
+		this.components = List.list(left, right);
+	}
+
+	@Override
+	public String expression()
+	{
+		return components.tail().foldLeft(
+				new F2<StringBuilder, Sequenceable, StringBuilder>() 
+				{
+					@Override
+					public StringBuilder f(final StringBuilder a, final Sequenceable b)
+					{return a.append(b.expression());}
+				}, 
+				new StringBuilder(components.head().expression())
+			).toString();
+	}
+
+	@Override
+	public Alternation or(final Alternation right) 
+	{return new Alternation(this, right);}
+
+	@Override
+	public Alternation or(final Alternative right) 
+	{return new Alternation(this, right);}
+	
+	@Override
+	public Sequence then(final Sequenceable right)
+	{return new Sequence(this, right);}
+
+	@Override
+	public Sequence then(final Sequence right)
+	{return new Sequence(this, right);}
+
+	public static interface Sequenceable extends Expression, SequenceOps
+	{}
+}
+
+
+
+interface SequenceOps
+{
+	Sequence then(Sequence.Sequenceable right);
+	Sequence then(Sequence right);
+}
