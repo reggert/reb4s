f9c22edcd77715f9fdcd2721f992855a8ee7c1a3
Richard W. Eggert II <richard.eggert@gmail.com>
2012-12-28 22:53:44 -0500

    implemented Raw
    GitHub ticket #5

--- net.sourceforge.reb4j/src/main/java/net/sourceforge/reb4j/Raw.java
+++ net.sourceforge.reb4j/src/main/java/net/sourceforge/reb4j/Raw.java
@@ -1,12 +1,15 @@
 package net.sourceforge.reb4j;
 
+import fj.F2;
+import fj.data.List;
+
 
 public class Raw extends AbstractSequenceableAlternative
 {
 	private static final long serialVersionUID = 1L;
 	private final String rawExpression;
 	
-	private Raw(final String rawExpression)
+	Raw(final String rawExpression)
 	{
 		assert rawExpression != null;
 		this.rawExpression = rawExpression;
@@ -15,6 +18,77 @@ public class Raw extends AbstractSequenceableAlternative
 	@Override
 	public final String expression()
 	{return rawExpression;}
+	
+	public static final class Compound extends Raw
+	{
+		private static final long serialVersionUID = 1L;
+		public final List<Raw> components;
+		
+		private Compound(final List<Raw> components)
+		{
+			super(compoundExpression(components));
+			this.components = components;
+		}
+		
+		private static String compoundExpression(final List<Raw> components)
+		{
+			return components.foldLeft(
+						new F2<StringBuilder, Raw, StringBuilder>()
+						{
+							@Override
+							public StringBuilder f(final StringBuilder a, final Raw b)
+							{return a.append(b);}
+						},
+						new StringBuilder()
+					)
+				.toString();
+		}
+		
+		@Override
+		public Compound then(final Raw right)
+		{
+			return new Compound(components.append(List.single(right)));
+		}
+		
+		@Override
+		public Compound then(final Compound right)
+		{
+			return new Compound(components.append(right.components));
+		}
+		
+	}
+	
+	
+	public static final class EscapedLiteral extends Raw
+	{
+		private static final long serialVersionUID = 1L;
+		public final Literal literal;
+		
+		private EscapedLiteral(final Literal literal)
+		{
+			super(literal.escaped());
+			this.literal = literal;
+		}
+	}
+	
+	
+	public Compound then(final Raw right)
+	{
+		return new Compound(List.list(this, right));
+	}
+	
+	
+	public Compound then(final Compound right)
+	{
+		return new Compound(right.components.cons(this));
+	}
+	
+	
+	public Compound then(final Literal right)
+	{
+		return this.then(new EscapedLiteral(right));
+	}
 }
+
 
 
