0123feb464ac3ed48ddb934ed50014f2d40d951f
Richard W. Eggert II <richard.eggert@gmail.com>
2012-12-29 17:51:49 -0500

    added factory methods for SingleChar and MultiChar
    GitHub ticket #5

--- net.sourceforge.reb4j/src/main/java/net/sourceforge/reb4j/charclass/CharClass.java
+++ net.sourceforge.reb4j/src/main/java/net/sourceforge/reb4j/charclass/CharClass.java
@@ -1,7 +1,9 @@
 package net.sourceforge.reb4j.charclass;
 
-import fj.data.LazyString;
+import static fj.Ord.charOrd;
 import net.sourceforge.reb4j.AbstractQuantifiableSequenceableAlternative;
+import fj.data.LazyString;
+import fj.data.Set;
 
 /**
  * Base class representing an expression that matches a single character 
@@ -15,4 +17,15 @@ public abstract class CharClass extends AbstractQuantifiableSequenceableAlternat
 	@Override
 	public final LazyString expression()
 	{return independentForm();}
+	
+	public static SingleChar character(final char c)
+	{return new SingleChar(c);}
+	
+	public static MultiChar characters(final char... cs)
+	{
+		Set<Character> set = Set.empty(charOrd);
+		for (final char c : cs)
+			set = set.insert(c);
+		return new MultiChar(set);
+	}
 }
