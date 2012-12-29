d7860282e8b53d5846032bd2be8b2b24900def4b
Richard W. Eggert II <richard.eggert@gmail.com>
2012-12-29 17:19:11 -0500

    implemented hashCode and equals
    GitHub ticket #5

--- net.sourceforge.reb4j/src/main/java/net/sourceforge/reb4j/charclass/Intersection.java
+++ net.sourceforge.reb4j/src/main/java/net/sourceforge/reb4j/charclass/Intersection.java
@@ -72,5 +72,27 @@ public final class Intersection extends CharClass
 	{
 		return new Intersection(right.supersets.cons(left));
 	}
+
+	@Override
+	public int hashCode()
+	{
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + supersets.hashCode();
+		return result;
+	}
+
+	@Override
+	public boolean equals(final Object obj)
+	{
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		final Intersection other = (Intersection) obj;
+		return supersets.equals(other.supersets);
+	}
 }
 
--- net.sourceforge.reb4j/src/main/java/net/sourceforge/reb4j/charclass/Union.java
+++ net.sourceforge.reb4j/src/main/java/net/sourceforge/reb4j/charclass/Union.java
@@ -71,6 +71,28 @@ public final class Union extends CharClass
 	{
 		return new Union(List.list(left, right));
 	}
+
+	@Override
+	public int hashCode()
+	{
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + subsets.hashCode();
+		return result;
+	}
+
+	@Override
+	public boolean equals(final Object obj)
+	{
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		final Union other = (Union) obj;
+		return subsets.equals(other.subsets);
+	}
 	
 }
 
