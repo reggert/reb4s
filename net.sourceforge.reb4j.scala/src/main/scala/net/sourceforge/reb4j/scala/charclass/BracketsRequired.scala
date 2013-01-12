a7f2f196d6a9b9465bf1f5ede33446666d5fa9bb
Richard W. Eggert II <richard.eggert@gmail.com>
2013-01-11 21:16:19 -0500

    made BracketsRequired private to the package for GitHub ticket #8

--- net.sourceforge.reb4j.scala/src/main/scala/net/sourceforge/reb4j/scala/charclass/BracketsRequired.scala
+++ net.sourceforge.reb4j.scala/src/main/scala/net/sourceforge/reb4j/scala/charclass/BracketsRequired.scala
@@ -4,7 +4,7 @@ package net.sourceforge.reb4j.scala.charclass
  * Trait indicating that a character class expression must be enclosed
  * in square brackets when used as a regular expression.
  */
-trait BracketsRequired extends CharClass
+private[charclass] trait BracketsRequired extends CharClass
 {
 	final override lazy val independentForm = "[" + unitableForm + "]"
 }
