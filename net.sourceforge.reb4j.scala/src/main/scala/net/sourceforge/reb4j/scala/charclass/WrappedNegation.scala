5d581c2465d2cad2fe98be659197687983a29452
Richard W. Eggert II <richard.eggert@gmail.com>
2013-01-12 02:14:00 -0500

    made WrappedNegation package private for GitHub ticket #8

--- net.sourceforge.reb4j.scala/src/main/scala/net/sourceforge/reb4j/scala/charclass/WrappedNegation.scala
+++ net.sourceforge.reb4j.scala/src/main/scala/net/sourceforge/reb4j/scala/charclass/WrappedNegation.scala
@@ -4,7 +4,7 @@ package net.sourceforge.reb4j.scala.charclass
  * Trait marking character classes that must be wrapped in
  * square brackets in order to be negated.
  */
-trait WrappedNegation[T <: WrappedNegation[T]] extends CharClass
+private[charclass] trait WrappedNegation[T <: WrappedNegation[T]] extends CharClass
 {
 	this : T =>
 	override final def negated = new Negated[T](this)
