c70b30db6a06635362cec629247339d2162952f5
Richard W. Eggert II <richard.eggert@gmail.com>
2013-01-20 16:27:35 -0500

    merged Alternation.Ops into Alternative
    Eliminating superfluous "Ops" traits (GitHub issue #14)

--- src/main/scala/net/sourceforge/reb4j/scala/Alternative.scala
+++ src/main/scala/net/sourceforge/reb4j/scala/Alternative.scala
@@ -5,10 +5,33 @@ package net.sourceforge.reb4j.scala
  * Interface representing an expression that can be used as an alternative
  * in an alternation expression.
  */
-trait Alternative extends Expression with Alternation.Ops
+trait Alternative extends Expression
 {
-	override final def || (right : Alternation) = 
-			new Alternation(this +: right.alternatives)
-	override final def || (right : Alternative) = 
+	/**
+	 * Constructs an expression matching either the receiver or the 
+	 * any of the alternatives contained within the specified argument 
+	 * expression.
+	 */
+	def || (right : Alternation) : Alternation =
+		new Alternation(this +: right.alternatives)
+		
+	/**
+	 * Constructs an expression matching either the receiver or the 
+	 * specified argument expression.
+	 */
+	def || (right : Alternative) : Alternation =
 		new Alternation(List(this, right))
+		
+	/**
+	 * Constructs an expression matching either the receiver or the 
+	 * any of the alternatives contained within the specified argument 
+	 * expression.
+	 */
+	final def or (right : Alternation) = this || right
+		
+	/**
+	 * Constructs an expression matching either the receiver or the 
+	 * specified argument expression.
+	 */
+	final def or (right : Alternative) = this || right
 }
