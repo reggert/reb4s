92f7fcf59fbfc8533396448c1e1081edd3f3765e
Richard W. Eggert II <richard.eggert@gmail.com>
2013-01-20 16:27:35 -0500

    merged Alternation.Ops into Alternative
    Eliminating superfluous "Ops" traits (GitHub issue #14)

--- net.sourceforge.reb4j.scala/src/main/scala/net/sourceforge/reb4j/scala/Alternation.scala
+++ net.sourceforge.reb4j.scala/src/main/scala/net/sourceforge/reb4j/scala/Alternation.scala
@@ -6,7 +6,7 @@ package net.sourceforge.reb4j.scala
 @SerialVersionUID(1L)
 final class Alternation private[scala] (val alternatives : List[Alternative]) 
 	extends Expression
-	with Alternation.Ops
+	with Alternative
 {
 	override lazy val expression = alternatives.mkString("|")
 	
@@ -20,43 +20,4 @@ final class Alternation private[scala] (val alternatives : List[Alternative])
 	}
 	
 	override lazy val hashCode = 31 * alternatives.hashCode
-}
-
-
-/**
- * Module providing the implementation of alternation.
- */
-object Alternation
-{
-	/**
-	 * Interface providing the operations that relate to alternation.
-	 */
-	trait Ops extends Expression
-	{
-		/**
-		 * Constructs an expression matching either the receiver or the 
-		 * any of the alternatives contained within the specified argument 
-		 * expression.
-		 */
-		def || (right : Alternation) : Alternation
-		
-		/**
-		 * Constructs an expression matching either the receiver or the 
-		 * specified argument expression.
-		 */
-		def || (right : Alternative) : Alternation
-		
-		/**
-		 * Constructs an expression matching either the receiver or the 
-		 * any of the alternatives contained within the specified argument 
-		 * expression.
-		 */
-		final def or (right : Alternation) = this || right
-		
-		/**
-		 * Constructs an expression matching either the receiver or the 
-		 * specified argument expression.
-		 */
-		final def or (right : Alternative) = this || right
-	}
 }
