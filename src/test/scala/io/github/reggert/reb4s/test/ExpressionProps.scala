66045246e14b722686740c2b91d50939fea934bc
Richard W. Eggert II <richard.eggert@gmail.com>
2013-11-10 17:36:04 -0500

    made ExpressionProps more specific about the type of exception to watch for

--- src/test/scala/io/github/reggert/reb4s/test/ExpressionProps.scala
+++ src/test/scala/io/github/reggert/reb4s/test/ExpressionProps.scala
@@ -1,14 +1,13 @@
 package io.github.reggert.reb4s.test
 
 import scala.util.Try
-
 import org.scalacheck.Prop.forAll
 import org.scalacheck.Properties
-
 import io.github.reggert.reb4s.Expression
+import java.util.regex.PatternSyntaxException
 
 object ExpressionProps extends Properties("Expression") with ExpressionGenerators {
 	property("toPattern") = forAll {e : Expression =>
-		Try(e.toPattern).isSuccess
+		try {e.toPattern; true} catch {case e : PatternSyntaxException => false}
 	}
 }
