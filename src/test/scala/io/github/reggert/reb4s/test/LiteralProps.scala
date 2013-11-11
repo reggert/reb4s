3018b1d1424eaf070267a2624f662797de73078d
Richard W. Eggert II <richard.eggert@gmail.com>
2013-11-10 23:39:30 -0500

    added LiteralProps

---
+++ src/test/scala/io/github/reggert/reb4s/test/LiteralProps.scala
@@ -0,0 +1,25 @@
+package io.github.reggert.reb4s.test
+
+import org.scalacheck.{Arbitrary, Properties}
+import Arbitrary.arbitrary
+import io.github.reggert.reb4s.Literal
+import Literal.{CharLiteral, StringLiteral}
+
+object LiteralProps extends Properties("Literal")
+	with ExpressionProperties[Literal] with LiteralGenerators
+{
+	property("toPattern") = toPattern(arbitrary[Literal])
+}
+
+object CharLiteralProps extends Properties("CharLiteral")
+	with ExpressionProperties[CharLiteral] with LiteralGenerators
+{
+	property("toPattern") = toPattern(arbitrary[CharLiteral])
+}
+
+
+object StringLiteralProps extends Properties("StringLiteral")
+	with ExpressionProperties[StringLiteral] with LiteralGenerators
+{
+	property("toPattern") = toPattern(arbitrary[StringLiteral])
+}
