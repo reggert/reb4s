e145ab1906aa8037e6b1161fc815092c63b732b9
Richard W. Eggert II <richard.eggert@gmail.com>
2013-11-24 16:04:55 -0500

    de-parametrized Raw and Literal generators, and controlled recursion depth in Raw generators

--- src/test/scala/io/github/reggert/reb4s/test/LiteralGenerators.scala
+++ src/test/scala/io/github/reggert/reb4s/test/LiteralGenerators.scala
@@ -6,20 +6,17 @@ import io.github.reggert.reb4s.Literal
 import Literal.{CharLiteral, StringLiteral}
 
 trait LiteralGenerators {
-	implicit val arbCharLiteral = Arbitrary(genCharLiteral())
-	implicit val arbStringLiteral = Arbitrary(genStringLiteral())
-	implicit val arbLiteral = Arbitrary(genLiteral())
+	implicit val arbCharLiteral = Arbitrary(genCharLiteral)
+	implicit val arbStringLiteral = Arbitrary(genStringLiteral)
+	implicit val arbLiteral = Arbitrary(genLiteral)
 	
 	
-	def genCharLiteral(charGen : => Gen[Char] = arbitrary[Char]) : Gen[CharLiteral] = 
-		for {c <- charGen} yield CharLiteral(c)
+	def genCharLiteral : Gen[CharLiteral] = 
+		for {c <- arbitrary[Char]} yield CharLiteral(c)
 	
-	def genStringLiteral(stringGen : => Gen[String] = arbitrary[String]) : Gen[StringLiteral] = 
-		for {s <- stringGen} yield StringLiteral(s)
+	def genStringLiteral : Gen[StringLiteral] = 
+		for {s <- arbitrary[String]} yield StringLiteral(s)
 	
-	def genLiteral(
-			charLiteralGen : => Gen[CharLiteral] = arbitrary[CharLiteral], 
-			stringLiteralGen : => Gen[StringLiteral] = arbitrary[StringLiteral]
-		) : Gen[Literal] = 
-		Gen.oneOf(charLiteralGen, stringLiteralGen)
+	def genLiteral : Gen[Literal] = 
+		Gen.oneOf(genCharLiteral, genStringLiteral)
 }
--- src/test/scala/io/github/reggert/reb4s/test/RawGenerators.scala
+++ src/test/scala/io/github/reggert/reb4s/test/RawGenerators.scala
@@ -7,25 +7,24 @@ import org.scalacheck.Gen
 import io.github.reggert.reb4s.{AnyChar, CompoundRaw, EscapedLiteral, InputBegin, InputEnd, InputEndSkipEOL, LineBegin, LineEnd, Literal, MatchEnd, NonwordBoundary, Quantifiable, Raw, WordBoundary}
 
 trait RawGenerators extends UtilGenerators with LiteralGenerators {
-	implicit val arbRawQuantifiable = Arbitrary(genRawQuantifiable())
-	implicit val arbCompoundRaw : Arbitrary[CompoundRaw] = Arbitrary(genCompoundRaw())
-	implicit val arbEscapedLiteral = Arbitrary(genEscapedLiteral())
-	implicit val arbRaw = Arbitrary(genRaw())
+	implicit val arbRawQuantifiable = Arbitrary(genRawQuantifiable)
+	implicit val arbCompoundRaw : Arbitrary[CompoundRaw] = Arbitrary(Gen.sized {depth => genCompoundRaw(depth)})
+	implicit val arbEscapedLiteral = Arbitrary(genEscapedLiteral)
+	implicit val arbRaw = Arbitrary(Gen.sized {depth => genRaw(depth)})
 	
 
-	def genCompoundRaw(rawGen : => Gen[Raw] = arbitrary[Raw]) : Gen[CompoundRaw] = for {
-		first <- rawGen
-		otherComponents <- genNonEmptyRecursiveList(rawGen)
+	def genCompoundRaw(depth : Int) : Gen[CompoundRaw] = for {
+		first <- genRaw(depth)
+		otherComponents <- genNonEmptyRecursiveList(genRaw(depth))
 	} yield CompoundRaw(first::otherComponents)
 	
-	def genRaw(
-			escapedLiteralGen : => Gen[EscapedLiteral] = arbitrary[EscapedLiteral], 
-			rawQuantifiableGen : => Gen[Raw with Quantifiable] = arbitrary[Raw with Quantifiable], 
-			compoundRawGen : => Gen[CompoundRaw] = Gen.lzy(arbitrary[CompoundRaw])
-		) : Gen[Raw] = 
-		Gen.frequency(2 -> escapedLiteralGen, 2 -> rawQuantifiableGen, 1 -> compoundRawGen)
+	def genRaw(depth : Int) : Gen[Raw] = 
+		if (depth == 0)
+			Gen.oneOf(genEscapedLiteral, genRawQuantifiable)
+		else
+			genCompoundRaw(depth - 1)
 	
-	def genRawQuantifiable() : Gen[Raw with Quantifiable] = Gen.oneOf(
+	def genRawQuantifiable : Gen[Raw with Quantifiable] = Gen.oneOf(
 			AnyChar,
 			LineBegin,
 			LineEnd,
@@ -37,8 +36,8 @@ trait RawGenerators extends UtilGenerators with LiteralGenerators {
 			InputEnd
 		)
 		
-	def genEscapedLiteral(literalGen : => Gen[Literal] = arbitrary[Literal]) : Gen[EscapedLiteral] = 
-		literalGen map {EscapedLiteral}
+	def genEscapedLiteral : Gen[EscapedLiteral] = 
+		genLiteral map {EscapedLiteral}
 }
 
 
