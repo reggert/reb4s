e9b1b2e57fe8dd47a4fb52e181052b06e96fdaa4
Richard W. Eggert II <richard.eggert@gmail.com>
2014-02-17 22:44:33 -0500

    consolidated look-behind tests into LookBehindProps
    GitHub issue #5 - PatternSyntaxException: Look-behind group does not have an obvious maximum length

---
+++ src/test/scala/io/github/reggert/reb4s/test/LookBehindProps.scala
@@ -0,0 +1,49 @@
+package io.github.reggert.reb4s.test
+
+import org.scalacheck.Arbitrary.arbitrary
+import org.scalacheck.Properties
+import io.github.reggert.reb4s.{Expression,Group}
+import Group.{PositiveLookBehind,NegativeLookBehind}
+import org.scalacheck.{Gen,Prop}
+import Prop.forAll
+import scala.util.Try
+import io.github.reggert.reb4s.UnboundedLookBehindException
+import io.github.reggert.reb4s.Quantified
+
+
+
+sealed abstract class LookBehindProps[GroupType <: Group](name : String)(constructor : Expression => GroupType) 
+	extends Properties(name)
+	with ExpressionGenerators with ExpressionShrinkers
+{
+	def genIndefinite(size : Int) : Gen[Quantified] = {
+		require(size > 0)
+		for {
+			mode <- Gen.oneOf(Quantified.Greedy, Quantified.Reluctant, Quantified.Possessive)
+			quantifiableGen = genQuantifiable(size)
+			quantified <- Gen.oneOf(
+					quantifiableGen map (_.anyTimes(mode)),
+					quantifiableGen map (_.atLeastOnce(mode)),
+					(for {
+						n <- arbitrary[Int] if n > 0
+						quantifiable <- quantifiableGen
+					} yield quantifiable.repeat(n, mode)),
+					(for {
+						n <- arbitrary[Int] if n > 0
+						quantifiable <- quantifiableGen	
+					} yield quantifiable.atLeast(n, mode))
+				)
+		} yield quantified
+	}
+	
+	property("UnboundedLookBehindException") = forAll(Gen.sized(n => genIndefinite(n + 1))){nested =>
+		Prop.throws(classOf[UnboundedLookBehindException])(constructor(nested))
+	}
+}
+
+object UnboundedPositiveLookBehindProps extends LookBehindProps("UnboundedPositiveLookBehind")(PositiveLookBehind)
+
+object UnboundedNegativeLookBehindProps extends LookBehindProps("UnboundedNegativeLookBehind")(NegativeLookBehind) 
+
+
+
