f9c7a308567a2bbcb6110bb95e887260afdc3825
Richard W. Eggert II <richard.eggert@gmail.com>
2014-02-17 23:12:09 -0500

    added isBounded filters to look-behind generators
    GitHub issue #5 - PatternSyntaxException: Look-behind group does not have an obvious maximum length

--- src/test/scala/io/github/reggert/reb4s/test/ExpressionGenerators.scala
+++ src/test/scala/io/github/reggert/reb4s/test/ExpressionGenerators.scala
@@ -117,10 +117,10 @@ trait ExpressionGenerators extends CharClassGenerators
 					expressionGen.map(Group.Capture),
 					expressionGen.map(Group.Independent),
 					expressionGen.map(Group.NegativeLookAhead),
-					expressionGen.map(Group.NegativeLookBehind),
+					expressionGen.filter(_.isBounded).map(Group.NegativeLookBehind),
 					expressionGen.map(Group.NonCapturing),
 					expressionGen.map(Group.PositiveLookAhead),
-					expressionGen.map(Group.PositiveLookBehind),
+					expressionGen.filter(_.isBounded).map(Group.PositiveLookBehind),
 					(for {
 						flags <- Gen.choose(0, 6) flatMap {flagCount => Gen.listOfN(flagCount, genFlag)}
 						genEnabled = Gen.lzy(expressionGen map {e : Expression => Group.EnableFlags(e, flags : _*)})
