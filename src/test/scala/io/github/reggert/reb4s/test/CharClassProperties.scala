704bb7df818ccb27a89837595af7d5b8c626215e
Richard W. Eggert II <richard.eggert@gmail.com>
2014-03-02 16:58:40 -0500

    passed Shrink implicited to toPattern and symmetricNegation properties

--- src/test/scala/io/github/reggert/reb4s/test/CharClassProperties.scala
+++ src/test/scala/io/github/reggert/reb4s/test/CharClassProperties.scala
@@ -1,9 +1,11 @@
 package io.github.reggert.reb4s.test
 
 import io.github.reggert.reb4s.charclass.CharClass
-import org.scalacheck.Gen
+import org.scalacheck.{Gen, Shrink}
+import Shrink._
 import org.scalacheck.Prop.forAll
 
 trait CharClassProperties[E <: CharClass] {
-	def symmetricNegation(g : Gen[E]) = forAll(g) {e => e.negated.negated == e}
+	def symmetricNegation(g : Gen[E])(implicit s : Shrink[E]) = 
+		forAll(g) {e => e.negated.negated == e}
 }
--- src/test/scala/io/github/reggert/reb4s/test/ExpressionProperties.scala
+++ src/test/scala/io/github/reggert/reb4s/test/ExpressionProperties.scala
@@ -2,11 +2,13 @@ package io.github.reggert.reb4s.test
 
 import java.util.regex.PatternSyntaxException
 
-import org.scalacheck.Gen
+import org.scalacheck.{Gen, Shrink}
+import Shrink._
 import org.scalacheck.Prop.forAll
 
 import io.github.reggert.reb4s.Expression
 
 trait ExpressionProperties[E <: Expression] {
-	def toPattern(g : Gen[E]) = forAll(g) {e : E => e.toPattern; true}
+	def toPattern(g : Gen[E])(implicit s: Shrink[E]) = 
+		forAll(g) {e : E => e.toPattern; true}
 }
